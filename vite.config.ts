2b2e77bae86ce8ddfe7beecec14a4fd015644fd4
lucas.rimbault <lucas.rimbault@tentelemed.com>
2024-11-14 17:09:57 +0100

    add vite-plugin-pwa package

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Mon Application PWA',
+      short_name: 'MonApp',
+      description: 'Une application React Vite transformée en PWA',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      scope: '/',
+      start_url: '/',
+      icons: [
+        {
+          src: '/logo.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/logo.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+  }),],
 })
